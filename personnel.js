2de56e95b518e0c0fe1c4aca73cf65c5923bb9fb
tauterlion <87108067+tauterlion@users.noreply.github.com>
2026-09-07 14:43:06 -0400

    Fix personnel portrait paths for GitHub Pages

--- personnel.js
+++ personnel.js
@@ -15,7 +15,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Life is for the living so you gotta live it up",
     "codename": "Professor",
-    "photo": "/assets/portraits/rs-0302.png"
+    "photo": "./assets/portraits/rs-0302.png"
   },
   "RS-1112": {
     "name": "Marco Valiente",
@@ -28,7 +28,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Did you know that tires can sometimes produce bones? Right here, this land, a rare scene of the blue screen meteor.",
     "codename": "Cruise",
-    "photo": "/assets/portraits/rs-1112.png"
+    "photo": "./assets/portraits/rs-1112.png"
   },
   "RS-0114": {
     "name": "Jeidenne Nieves",
@@ -41,7 +41,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "The chain is as strong as the weakest link.",
     "codename": "Nimbus",
-    "photo": "/assets/portraits/rs-0114.png"
+    "photo": "./assets/portraits/rs-0114.png"
   },
   "RS-0223": {
     "name": "Nicholas Fuell",
@@ -54,7 +54,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Nico’s law states: Nico is never wrong and always factually correct.",
     "codename": "King Frog",
-    "photo": "/assets/portraits/rs-0223.png"
+    "photo": "./assets/portraits/rs-0223.png"
   },
   "RS-0712": {
     "name": "Todd Andersen",
@@ -67,7 +67,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "The Cagoon is the best place for peace and serenity.",
     "codename": "Torpedo",
-    "photo": "/assets/portraits/rs-0712.png"
+    "photo": "./assets/portraits/rs-0712.png"
   },
   "RS-0501": {
     "name": "Kamilo Sepulveda",
@@ -80,7 +80,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Hola.",
     "codename": "Bromista",
-    "photo": "/assets/portraits/rs-0501.png"
+    "photo": "./assets/portraits/rs-0501.png"
   },
   "RS-0120": {
     "name": "Sebastian Machicote",
@@ -93,7 +93,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Whatever a man’s mind can conceive and believe, he can achieve.",
     "codename": "Buena-Vibra",
-    "photo": "/assets/portraits/rs-0120.png"
+    "photo": "./assets/portraits/rs-0120.png"
   },
   "RS-0518": {
     "name": "Dash Greenberg",
@@ -106,7 +106,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Don’t be a product of your environment; produce your own.",
     "codename": "Yankee",
-    "photo": "/assets/portraits/rs-0518.png"
+    "photo": "./assets/portraits/rs-0518.png"
   },
   "RS-1128": {
     "name": "Alexander Flachowsky",
@@ -119,7 +119,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "There’s never enough gas; the more the merrier.",
     "codename": "Analyst",
-    "photo": "/assets/portraits/rs-1128.png"
+    "photo": "./assets/portraits/rs-1128.png"
   },
   "RS-0202": {
     "name": "Jaime Del Valle",
@@ -132,7 +132,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "[NO QUOTE, KEPT CHEWING]",
     "codename": "Giga-Man",
-    "photo": "/assets/portraits/rs-0202.png"
+    "photo": "./assets/portraits/rs-0202.png"
   },
   "RS-0316": {
     "name": "Shane Smater",
@@ -145,7 +145,7 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "Hey.",
     "codename": "Siphon",
-    "photo": "/assets/portraits/rs-0316.png"
+    "photo": "./assets/portraits/rs-0316.png"
   },
   "RS-0311": {
     "name": "Maria Trebilcock",
@@ -158,6 +158,6 @@ window.REDSCAPES_PERSONNEL = {
     "expiration": "11/2008",
     "quote": "",
     "codename": "Inkwraith",
-    "photo": "/assets/portraits/rs-0311.png"
+    "photo": "./assets/portraits/rs-0311.png"
   }
 };
